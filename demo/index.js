c6d94199f8e846b955455258fe5a8fc08aa600fa
Draltan Marin <moi@draltan.com>
2019-01-24 22:54:40 +0100

    chore: project setup

---
+++ demo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render} from 'react-dom';
+
+// import from '../src';
+
+const App = () => (
+  <div>
+  </div>
+);
+
+render(<App />, document.getElementById('root'));
