7b0bea3cfb057c012bd99803caee0a30906aacda
Draltan Marin <moi@draltan.com>
2019-01-25 00:55:41 +0100

    feat: implement basic renderers for primitive types

--- demo/index.js
+++ demo/index.js
@@ -6,13 +6,35 @@ import { GenericData } from '../src';
 const myObject = {
   a: 42,
   b: 'Foo',
-  c: [1, 1, 3, 5, 8],
-  d: { key: 'value' },
+  c: [1, 1, 2, 3, 5, 8],
+  d: { shortText: 'value', longText: `Some
+big
+text
+` },
 };
+
+const myString = 'foo';
+const myMultilineString = `foo
+bar
+buz
+`;
 
 const App = () => (
   <div>
+    <h1>object</h1>
     <GenericData data={myObject}/>
+    <h1>string</h1>
+    <GenericData data={myString}/>
+    <h1>multi line string</h1>
+    <GenericData data={myMultilineString}/>
+    <h1>array</h1>
+    <GenericData data={[1, 2, 3]}/>
+    <h1>integers</h1>
+    <GenericData data={11235813}/>
+    <h1>decimal</h1>
+    <GenericData data={1142.3456}/>
+    <h1>boolean</h1>
+    <GenericData data={true}/>
   </div>
 );
 
--- src/GenericData.js
+++ src/GenericData.js
@@ -1,9 +1,106 @@
 import React from 'react';
 
-const GenericData = ({
+const GenericData = ({data}) => {
+  if (data === null) return <NullFormat/>;
+  switch (typeof data) {
+  case 'string':
+    return <StringFormat data={data}/>;
+  case 'object':
+    if (Array.isArray(data)) {
+      return <ArrayFormat data={data}/>;
+    } else { // object
+      return <ObjectFormat data={data}/>;
+    }
+  case 'boolean':
+    return <BooleanFormat data={data}/>;
+  case 'number':
+    return <NumberFormat data={data}/>;
+  case 'symbol':
+  case 'function':
+  case 'undefined':
+    return <UndefinedFormat/>;
+  default:
+    return <span/>;
+  }
+};
+
+const ObjectFormat = ({
   data = {}
+}) => {
+  return (
+    <ul>{
+      Object.entries(data).map(
+        ([key, value]) => (
+          <li key={key}>
+            <label>{key}</label>
+            {': '}
+            <GenericData data={value}/>
+          </li>
+        )
+      )
+    }</ul>
+  );
+};
+
+const ArrayFormat = ({
+  data = {}
+}) => {
+  return (
+    <ul>{
+      data.map(
+        (value, index) => (
+          <li key={index}>
+            <GenericData data={value}/>
+          </li>
+        )
+      )
+    }</ul>
+  );
+};
+
+const newLine = /\n/;
+
+const StringFormat = ({
+  data = ''
+}) => {
+  return newLine.test(data)? (
+    <pre>
+      <code>
+        {data}
+      </code>
+    </pre>
+  ) : (
+    <span>
+      {data}
+    </span>
+  );
+};
+
+const numberFormatter = new Intl.NumberFormat('fr-FR', { maximumFractionDigits: 2 });
+const NumberFormat = ({
+  data = 0
+}) => {
+  return (
+    <span>
+      {numberFormatter.format(data)}
+    </span>
+  );
+};
+
+const BooleanFormat = ({
+  data = true
 }) => (
-  <div>{JSON.stringify(data)}</div>
+  <code>
+    {data.toString()}
+  </code>
+);
+
+const UndefinedFormat = () => (
+  <code>undefined</code>
+);
+
+const NullFormat = () => (
+  <code>null</code>
 );
 
 export default GenericData;
